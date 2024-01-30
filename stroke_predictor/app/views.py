9e53ecbfb1f4961577b2fb4383fd88b9c0f5353e
MatanBuljubasic <93148670+MatanBuljubasic@users.noreply.github.com>
2024-01-30 19:50:01 +0100

    Update views.py

--- stroke_predictor/app/views.py
+++ stroke_predictor/app/views.py
@@ -99,12 +99,15 @@ def result(request):
         payload[request.POST['smoking_status']] = 1
 
         ######## API zahtjev #############
+        stroke, probabilities = callEndpoint(payload=payload)
+
+        probabilities = round(probabilities * 3)
 
         percentages = getPercentageFromInterval(formAge, formBMI, formGlucose)
     
         context = {
-            "stroke": 0, #ovo treba bit iz apia
-            "neighbors": 3, #ovo treba bit iz apia
+            "stroke": stroke, #ovo treba bit iz apia
+            "neighbors": probabilities, #ovo treba bit iz apia
             "age": formAge,
             "bmi": formBMI,
             "avg_glucose_level": formGlucose,
@@ -127,6 +130,58 @@ def result(request):
     
     else:
         return HttpResponseRedirect(reverse('app:home'))
+    
+def callEndpoint(payload):
+    import urllib.request
+    import json
+    import os
+    import ssl
+
+    def allowSelfSignedHttps(allowed):
+        # bypass the server certificate verification on client side
+        if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+            ssl._create_default_https_context = ssl._create_unverified_context
+
+    allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+    # Request data goes here
+    # The example below assumes JSON formatting which may be updated
+    # depending on the format your endpoint expects.
+    # More information can be found here:
+    # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+    data =  {
+    "Inputs": {
+        "input1": [payload]
+    },
+    "GlobalParameters": {}
+    }
+
+    body = str.encode(json.dumps(data))
+
+    url = 'http://5d714d01-af39-4c7c-bae7-6b6ee6aa91df.westeurope.azurecontainer.io/score'
+    # Replace this with the primary/secondary key or AMLToken for the endpoint
+    api_key = 'CVpXqhNPQdgd72CLkilCWrLmbYCrpZYq'
+    if not api_key:
+        raise Exception("A key should be provided to invoke the endpoint")
+
+
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+
+        result = response.read().decode('utf-8')
+        json_obj = json.loads(result)
+        return [json_obj['Results']['WebServiceOutput0'][0]['Scored Labels'], json_obj['Results']['WebServiceOutput0'][0]['probabilities']]
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(error.read().decode("utf8", 'ignore'))
+    
 
 
 
